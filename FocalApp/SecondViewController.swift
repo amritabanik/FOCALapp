dabdd59e9e4315c075f5cde17c8af79d8c91dec8
Selin Acar <sacar2@uwo.ca>
2016-03-05 18:20:20 -0500

    Initial Commit

---
+++ FocalApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FocalApp
+//
+//  Created by selin acar on 2016-03-05.
+//  Copyright © 2016 selin acar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
